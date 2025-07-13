6ca20e7b06384d87d38a5dcdcb9c2e4147a9850d
yogesh-chaturvedi <yogeshchaturvedi2425@gmail.com>
2025-07-13 23:19:48 +0530

    Deleted logo

--- frontend/src/assets/Assets.js
+++ frontend/src/assets/Assets.js
@@ -1,11 +1,9 @@
-import logo from '../assets/ShopNest-logo.png';
 import banner from '../assets/banner.jpg';
 import tshirt from '../assets/tshirt.webp';
 import cloths from '../assets/cloths.jpg';
 import contactImg from '../assets/contact.jpeg';
 
 export const assets = {
-    logo,
     banner,
     tshirt,
     cloths,
