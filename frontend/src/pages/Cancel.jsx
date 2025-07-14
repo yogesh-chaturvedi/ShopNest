225f902e22f6a19d363a65a5dad1e4fe53275a43
yogesh-chaturvedi <yogeshchaturvedi2425@gmail.com>
2025-07-14 11:52:05 +0530

    Fix Cancel page

--- frontend/src/pages/Cancel.jsx
+++ frontend/src/pages/Cancel.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Cancel = () => {
+    let naviagte = useNavigate()
+
+    function handleNavigation() {
+        naviagte('/')
+    }
+
+
     return (
-        <div className='flex items-center justify-center bg-green-200 min-h-screen '>
-            <div className="box border border-black bg-green-500 text-center p-5 rounded-2xl h-[190px] w-[300px]">
-                <h1 className='font-bold text-2xl '>Payment Failed</h1>
-                <p className="mt-4 text-lg">Wait, and try again later</p>
+        <div className='flex items-center justify-center bg-gradient-to-br from-green-100 to-green-200 min-h-screen'>
+
+            <ToastContainer position="top-center" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick={false} rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="colored" />
+
+            <div className="box border border-green-400 bg-white text-center p-6 rounded-2xl shadow-xl h-[200px] w-[320px]">
+                <h1 className='font-bold text-2xl text-green-700'>Payment Failed!</h1>
+                <p className="mt-4 text-gray-700 text-lg">Try Again Later</p>
+                <button onClick={() => { handleNavigation() }} className='text-white text-lg font-semibold px-4 py-2 mt-6 bg-green-600 hover:bg-green-700 transition-colors duration-200 rounded-xl'>
+                    Go To Home
+                </button>
             </div>
         </div>
     )
